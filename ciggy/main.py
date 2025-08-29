6c711430575dfffe9fdfdc3577ffb6a56b8581b6
debianell <debianell@debianell.debianell>
2025-08-29 15:20:44 +0100

    from functional to OOP

--- ciggy/main.py
+++ ciggy/main.py
@@ -2,28 +2,65 @@ from ciggy.config import bookmarks_path
 from bs4 import BeautifulSoup as bs
 
 
+class CiggyAttrs():
+    def __init__(self, attrs: dict = dict(), text: str = str):
+        self.attrs = attrs
+        self.text = text
+        self.__set_init_attrs()
+
+    def __set_init_attrs(self):
+        for attr, value in self.attrs.items():
+            setattr(self, attr, value)
+
+    def __repr__(self):
+        output = 'CiggyAttrs(\n'
+        for key in self.attrs.keys():
+            output += f'\t{key}: {getattr(self, key)}\n'
+        output += ')'
+        return output
+
+
+class Ciggy():
+    def __init__(self, tags: set = set(), file_name: str = str()):
+        self.tags = tags
+        self.title = file_name
+        self.__set_init_tags()
+
+    def __set_init_tags(self):
+        for tag in self.tags:
+            setattr(self, tag, None)
+
+    def set_tag_value(self, tag_str, value):
+        setattr(self, tag_str, value)
+
+    def __repr__(self):
+        output = 'Ciggy('
+        for tag in self.tags:
+            output += f'{tag}, '
+        output += ')'
+        return output
+
+
 def _file_parsing(content):
     """Given the file descriptor parse the page and extract the content.
     Mainly title and s tags
     """
-    results = {}
-    atext = []
-    aattrs = []
     soup = bs(content, 'lxml')
     tags = set([tag.name for tag in soup.find_all()])
+    cg = Ciggy(file_name="hello", tags=tags)
     for tag in tags:
-        if tag == 'title' or tag == 'h1':
-            results[tag] = soup.find(tag).text
-        if tag == 'a':
-            results[tag] = {}
-            for a_tag in soup.find_all(tag):
-                atext.append(a_tag.text)
-                aattrs.append(a_tag.attrs)
-            results[tag].update({
-                'text': atext,
-                'attrs': aattrs
-            })
-    return results
+        all_tags = soup.find_all(tag)
+        if len(all_tags) == 1:
+            try:
+                cg.set_tag_value(tag, all_tags[0].text)
+            except AttributeError:
+                continue
+        else:
+            cg.set_tag_value(
+                tag,
+                [CiggyAttrs(at.attrs, at.text) for at in all_tags]
+            )
+    return cg
 
 
 def main():
@@ -35,7 +72,7 @@ def main():
                 with open(file, mode='r', encoding='utf-8') as f:
                     content = f.read()
                     file_content.update({
-                        'file_name': file,
+                        'file_name': str(file),
                         'data': _file_parsing(content)
                     })
                     results.append(file_content)
@@ -46,3 +83,5 @@ def main():
 
 if __name__ == '__main__':
     data = main()
+
+data = main()
